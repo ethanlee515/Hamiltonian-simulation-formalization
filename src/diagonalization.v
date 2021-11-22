de511097926a4cc6142675356f890d43e2e7cf2e
Connor Clayton <ConnorClayton@wireless-10-105-62-92.umd.edu>
2021-11-22 12:29:22 -0500

    our first proof!

--- src/diagonalization.v
+++ src/diagonalization.v
@@ -1,12 +1,37 @@
 Require Import Reals.
 Require Import QWIRE.Matrix.
+Require Import Omega.
 
 Definition Herm {n: nat} (A : Square n) : Prop :=
   forall i j, (A i j) = Cconj (A j i).
 
+Lemma real_neg_neq : forall (x : R), x <> 0 -> x <> Ropp x.
+Proof.
+  intros. intros H1.
+  destruct (Rlt_dec x 0).
+  - assert (H2 : Ropp x > 0). {
+      apply Ropp_gt_lt_0_contravar in r. assumption. }.
+    rewrite <- H1 in H2.
+    apply Rlt_le in r. apply Rle_not_gt in r. contradiction.
+  - apply Rnot_lt_ge in n. apply Rge_le in n.
+    assert (H2 : 0 >= Ropp x). {
+      apply Ropp_0_le_ge_contravar in n. assumption. }.
+    assert (H3 : 0 < x). {
+      inversion n. assumption. symmetry in H0. contradiction. }.
+    rewrite <- H1 in H2. apply Rge_not_lt in H2.
+    contradiction.
+Qed.
+
 Lemma herm_diag_real {n : nat} (A : Square n) :
   forall (i : nat) (x : C), Herm A -> (i < n)%nat -> A i i = x -> snd x = 0.
-Proof. Admitted.
+Proof. intros. unfold Herm in H. unfold Cconj in H.
+       remember (H i i) as H2.
+       clear HeqH2. rewrite surjective_pairing in H1.
+       rewrite H1 in H2. inversion H2. 
+       destruct (Req_dec (snd x) 0).
+       - assumption.
+       - apply real_neg_neq in H3. contradiction.
+Qed.
 
 Definition Diagonal {n : nat} (A : Square n) :=
   forall i j, i <> j ->  A i j = 0.
