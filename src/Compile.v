bee07e7a6c6f91c832f3ec5e8f5d5b12b842917b
Yi Lee <ylee1228@umd.edu>
2021-12-01 13:26:16 +0000

    Add trotterize file

--- src/Compile.v
+++ src/Compile.v
@@ -4,6 +4,7 @@ Require Import List.
 Require Import HSF_Syntax.
 Require Import DecimalString.
 Require Import Qasm.
+Require Import Trotterize.
 
 Open Scope list_scope.
 Open Scope string_scope.
@@ -11,15 +12,6 @@ Open Scope string_scope.
 Import ListNotations.
 
 Definition parseProgram (program_text : string) : H_Program := makeHProg [] [].
-
-(* TODO Implement this *)
-(* Right now it maps everything to a fixed circuit *)
-Definition trotterize (program : H_Program) : QasmProgram := makeQasmProg 3
-[
-    QasmTerm1 (Rx 1) 0 ;
-    QasmTerm1 (Ry 1) 2 ;
-    QasmTerm2 (Rzz 1) 0 1
-].
 
 Definition compile (program_text : string) :=
     let program := parseProgram program_text in
