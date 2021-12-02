6144a9f50d34f5fba34777d8507ea14752e4ebda
Yi Lee <ylee1228@umd.edu>
2021-12-02 08:42:20 +0000

    Fix extraction

--- extracted/compile.ml
+++ extracted/compile.ml
@@ -11,14 +11,16 @@ let rec write_to_file chan lst =
             write_to_file chan tail
     | [] -> ()
 
-let compile in_filename out_filename dt =
+let compile in_filename out_filename numSlices =
     let in_file = open_in in_filename in
     let in_program_body = read_file_aux in_file in
-    let compilation_result = Compile_coq.compile in_program_body in
+    let compilation_result = Compile_coq.compile in_program_body numSlices in
     let out_file = open_out out_filename in
     write_to_file out_file compilation_result;;
 
+let s = Sys.argv.(3);;
+
 match Array.length Sys.argv with
-| 4 -> compile Sys.argv.(1) Sys.argv.(2) Sys.argv.(3)
+| 4 -> compile Sys.argv.(1) Sys.argv.(2) (List.init (String.length s) (String.get s))
 | _ -> Printf.printf "usage: ./compile program.ham program.qasm 1000\n"
 ;;
--- src/Compile.v
+++ src/Compile.v
@@ -1,11 +1,13 @@
-Require Import String Extraction ExtrOcamlString.
-Require Import ExtrOcamlNatInt.
+Require Import String.
+Require Import ExtrOcamlBasic.
+Require Import ExtrOcamlString.
 Require Import Reals.
 Require Import List.
 Require Import HSF_Syntax.
 Require Import DecimalString.
 Require Import Qasm.
 Require Import Trotterize.
+Require Import Decimal.
 
 Open Scope list_scope.
 Open Scope string_scope.
@@ -14,7 +16,13 @@ Import ListNotations.
 
 Definition parseProgram (program_text : string) : H_Program := makeHProg [] [].
 
-Definition compile (program_text : string) (nSlices : nat) :=
+Definition compile (program_text : string) (nSlices_text : string) :=
+    let nSlices := Nat.of_uint (
+        match (NilEmpty.uint_of_string nSlices_text) with
+        | Some x => x
+        | None => Nil
+        end
+    ) in
     let program := parseProgram program_text in
     let qasm := trotterize program nSlices in
     print_qasm_program qasm.
