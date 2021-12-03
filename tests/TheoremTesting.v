d1b36b49dd970c6750847da121ee35785ee49854
Connor Clayton <ConnorClayton@wireless-10-104-63-58.umd.edu>
2021-12-03 11:00:59 -0500

    cleaning up

--- tests/TheoremTesting.v
+++ tests/TheoremTesting.v
@@ -2,37 +2,6 @@ Set Warnings "-notation-overridden,-parsing".
 From Coq Require Export String.
 From HSF Require Import src.Semantics.
 
-(*
-Parameter MISSING: Type.
-
-Module Check.
-
-Ltac check_type A B :=
-    match type of A with
-    | context[MISSING] => idtac "Missing:" A
-    | ?T => first [unify T B; idtac "Type: ok" | idtac "Type: wrong - should be (" B ")"]
-    end.
-
-Ltac print_manual_grade A :=
-    match eval compute in A with
-    | Some (_ ?S ?C) =>
-        idtac "Score:"  S;
-        match eval compute in C with
-          | ""%string => idtac "Comment: None"
-          | _ => idtac "Comment:" C
-        end
-    | None =>
-        idtac "Score: Ungraded";
-        idtac "Comment: None"
-    end.
-
-End Check.
-
-
-From PLF Require Import Stlc.
-Import Check.
- *)
-
 Goal True.
 
 idtac "-------------------  commuting_Ham_semantics  --------------------".
@@ -46,6 +15,3 @@ idtac " ".
 
 Abort.
 
-(* 2021-11-14 17:52 *)
-
-(* 2021-11-14 17:52 *)
