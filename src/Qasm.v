1b09c964b768d890b0a4302687911a80fe4b6cdf
Yi Lee <ylee1228@umd.edu>
2021-12-04 14:17:42 -0500

    Implement first order Trotterization dt slicing

--- src/Qasm.v
+++ src/Qasm.v
@@ -2,14 +2,15 @@ Require Import Reals.
 Require Import String.
 Require Import QWIRE.Matrix.
 Require Import DecimalString.
-Open Scope string_scope.
+Require Import HSF_Syntax.
 
 Inductive QasmGate1 :=
-  | Rx (theta : R)
-  | Ry (theta : R)
-  | Rz (theta : R).
+  | Rx (theta : HScalar)
+  | Ry (theta : HScalar)
+  | Rz (theta : HScalar).
 
-Definition printQasmGate1 (g : QasmGate1) :=
+(* TODO *)
+Definition printQasmGate1 (g : QasmGate1) : string :=
     match g with
     | Rx theta => "rx(theta)"
     | Ry theta => "ry(theta)"
@@ -17,10 +18,11 @@ Definition printQasmGate1 (g : QasmGate1) :=
     end.
 
 Inductive QasmGate2 :=
-  | Rxx (theta : R)
-  | Rzz (theta : R).
+  | Rxx (theta : HScalar)
+  | Rzz (theta : HScalar).
 
-Definition printQasmGate2 (g : QasmGate2) :=
+(* TODO *)
+Definition printQasmGate2 (g : QasmGate2) : string :=
     match g with
     | Rxx theta => "rxx(theta)"
     | Rzz theta => "rzz(theta)"
@@ -29,11 +31,13 @@ Definition printQasmGate2 (g : QasmGate2) :=
 Definition string_of_nat (n : nat) := NilZero.string_of_int (Nat.to_int n).
 
 Inductive QasmTerm :=
-    | QasmTerm1 (gate : QasmGate1) (loc : nat)
-    | QasmTerm2 (gate : QasmGate2) (loc1 loc2 : nat).
+| QasmNoOp
+| QasmTerm1 (gate : QasmGate1) (loc : nat)
+| QasmTerm2 (gate : QasmGate2) (loc1 loc2 : nat).
 
-Definition printQasmTerm (t : QasmTerm) :=
-    match t with
+Definition printQasmTerm (t : QasmTerm) : string :=
+  match t with
+    | QasmNoOp => ""
     | QasmTerm1 gate loc =>
         printQasmGate1 gate ++ " sites[" ++ string_of_nat loc ++ "];\n"
     | QasmTerm2 gate loc1 loc2 =>
@@ -42,7 +46,7 @@ Definition printQasmTerm (t : QasmTerm) :=
             " sites[" ++ string_of_nat loc2 ++ "];\n"
     end.
 
-Fixpoint print_circuit (circuit : list QasmTerm) :=
+Fixpoint print_circuit (circuit : list QasmTerm) : string :=
     match circuit with
     | [] => ""
     | head :: tail => printQasmTerm head ++ (print_circuit tail)
@@ -54,17 +58,17 @@ Record QasmProgram := makeQasmProg {
 }.
 
 (* I would use OPENQASM 3 but Qiskit doesn't fully support it *)
-Definition make_qasm_header (numqubits : nat) :=
+Definition make_qasm_header (numqubits : nat) : string :=
     "OPENQASM 2.0;\n" ++
     "include ""qelib1.inc"";\n" ++
     "qreg sites[" ++ string_of_nat numqubits ++ "];\n" ++
     "reset sites;\n".
 
-Definition make_qasm_footer (numqubits : nat) :=
+Definition make_qasm_footer (numqubits : nat) : string :=
     "creg results[" ++ string_of_nat numqubits ++ "];\n" ++
     "measure sites -> results;\n".
 
-Definition print_qasm_program (prog : QasmProgram) :=
+Definition print_qasm_program (prog : QasmProgram) : string :=
     make_qasm_header prog.(num_qubits) ++
     print_circuit prog.(circuit) ++
     make_qasm_footer prog.(num_qubits).
--- src/Trotterize.v
+++ src/Trotterize.v
@@ -7,21 +7,107 @@ Require Import Semantics.
 Require Import PauliRotations.
 Require Import MatrixExponential.
 
+Definition find_qubit_opt (decls : list string) (site : string) : option nat :=
+  let loc := find_qubit decls site in
+  if Nat.eqb loc (List.length decls) then None else Some loc.
+
+
+Definition makeQT1 (p : Pauli) (theta : HScalar) (loc : nat) :=
+  match p with
+  | Pauli_I => []
+  | Pauli_X => [QasmTerm1 (Rx theta) loc]
+  | Pauli_Y => [QasmTerm1 (Ry theta) loc]
+  | Pauli_Z => [QasmTerm1 (Rz theta) loc]
+  end.
+
+Definition makeQT2_opt (p1 p2 : Pauli) (theta : HScalar) (loc1 loc2 : nat) :=
+  (* TODO *) Some [QasmTerm2 (Rxx theta) loc1 loc2].
+
+Definition natToHSc (n : nat) := (* TODO *) HScReal R1 "1".
+
+Definition sliceTerm (decls : list string) (duration : HScalar) (term : TIH_Term) (nSlices : nat) :=
+  let theta := HScDiv (HScMult duration term.(hScale)) (natToHSc nSlices) in
+  match term.(hPaulis) with
+  | [] => Some []
+  | [HIdOp site p] =>
+      match find_qubit_opt decls site with
+      | None => None
+      | Some loc => Some (makeQT1 p theta loc)
+      end
+  | [HIdOp site1 p1; HIdOp site2 p2] =>
+      match (find_qubit_opt decls site1, find_qubit_opt decls site2) with
+      | (Some loc1, Some loc2) => makeQT2_opt p1 p2 theta loc1 loc2
+      | _ => None
+      end
+  | _ => None (* Too nonlocal *)
+  end.
+
+
+Fixpoint sliceTerms (decls : list string)
+         (duration : HScalar)
+         (terms : list TIH_Term)
+         (nSlices : nat) :=
+  match terms with
+  | head :: tail =>
+      match (sliceTerm decls duration head nSlices, sliceTerms decls duration tail nSlices) with
+      | (Some head_insts, Some tail_insts) => Some (head_insts ++ tail_insts)
+      | _ => None
+      end
+  | [] => Some []
+  end.
+
+Print HSF_Term.
+
+Definition sliceInst (decls : list string) (inst : HSF_Term) (nSlices : nat) :=
+  sliceTerms decls inst.(Duration) inst.(Hamiltonian) nSlices.
+
+Fixpoint accSlices (slice : list QasmTerm) (nSlices : nat) :=
+  match nSlices with
+  | O => []
+  | S pr => slice ++ (accSlices slice pr)
+  end.
+
+Definition trotterizeInst (decls : list string) (inst : HSF_Term) (nSlices : nat) :=
+  match sliceInst decls inst nSlices with
+  | Some slice => Some (accSlices slice nSlices)
+  | None => None
+  end.
+
+Fixpoint trotterizeInsts (decls : list string) (insts : list HSF_Term) (nSlices : nat) :=
+  match insts with
+  | head :: tail =>
+      match (trotterizeInst decls head nSlices, trotterizeInsts decls tail nSlices) with
+      | (Some qasm_head, Some qasm_tail) => Some (qasm_head ++ qasm_tail)
+      | _ => None
+      end
+  | [] => Some []
+  end.
+
+Record trotterize_result := makeTrotRes
+{
+  successful : bool;
+  output : QasmProgram;
+}.
+
+Definition idProg := makeQasmProg 0 [].
+
+Definition trotterize (prog : H_Program) (nSlices : nat) :=
+  match trotterizeInsts prog.(Decls) prog.(Terms) nSlices with
+  | Some qasm_insts => makeTrotRes true (makeQasmProg (count_sites prog) qasm_insts)
+  | None => makeTrotRes false idProg
+  end.
+
 Definition QasmSemantics (prog : QasmProgram) : Square (2 ^ prog.(num_qubits)) :=
     (* TODO *) Zero.
 
-Definition trotterize (prog : H_Program) (slices : nat) : QasmProgram := makeQasmProg (count_sites prog)
-[
-    QasmTerm1 (Rx 1) 0 ;
-    QasmTerm1 (Ry 1) 2 ;
-    QasmTerm2 (Rzz 1) 0 1
-].
-
 Theorem trotterize_correct :
-    forall (hprog : H_Program),
-        let N := dims hprog in
+  forall (hprog : H_Program),
+      (forall nSlices, (trotterize hprog nSlices).(successful) = false) (* Cannot Trotterize *) \/
+      (forall nSlices, (trotterize hprog nSlices).(successful) = true (* Can Trotterize *) /\
         exists correct_sem,
-            sem_program hprog correct_sem /\
-            seq_conv (MatrixMetricSpace N) (fun nSlices => QasmSemantics (trotterize hprog nSlices)) correct_sem. 
+          sem_program hprog correct_sem /\
+          seq_conv (MatrixMetricSpace (dims hprog))
+            (fun nSlices => QasmSemantics (trotterize hprog nSlices).(output)) correct_sem
+      ).
 Proof.
     Admitted.
