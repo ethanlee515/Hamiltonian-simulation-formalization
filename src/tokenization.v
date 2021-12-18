5b2af80bd34fa588aafce6420175b11111d802e4
mshingan <manasi.mshingane@gmail.com>
2021-12-17 17:29:48 -0800

    parser attempt

--- src/tokenization.v
+++ src/tokenization.v
@@ -9,8 +9,9 @@ Require Import Bool BinPos BinNat PeanoNat Nnat Coq.Strings.Byte.
 Require Import Reals QWIRE.Matrix.
 Require Import DecimalString.
 Set Printing Projections.
+(*This file is an incomplete parser. *)
 
-(*tokenization*) 
+(*Checks whitespace*) 
 Definition isWhite (c : ascii) : bool :=
   let n := nat_of_ascii c in
   orb (orb (n =? 32) (* space *)
@@ -20,40 +21,50 @@ Definition isWhite (c : ascii) : bool :=
 
 Notation "x '<=?' y" := (x <=? y)
   (at level 70, no associativity) : nat_scope.
+
+(*Checks if character is lowercase letter*)
 Definition isLowerAlpha (c : ascii) : bool :=
   let n := nat_of_ascii c in
     andb (97 <=? n) (n <=? 122).
 
+(*checks if character is uppercase letter*)
 Definition isAlpha (c : ascii) : bool :=
   let n := nat_of_ascii c in
     orb (andb (65 <=? n) (n <=? 90))
         (andb (97 <=? n) (n <=? 122)).
 
+(*checks if character is an integer*) 
 Definition isDigit (c : ascii) : bool :=
   let n := nat_of_ascii c in
      andb (48 <=? n) (n <=? 57).
 
+(*checks equivalence arbitrary ascii character given ascii number *) 
 Definition isSingle_Char (c : ascii) (num :nat)  : bool :=
   let  n:= nat_of_ascii c in (n =? num).
 
+(*checks if character is a possible Pauli matrix*) 
 Definition isPauli (c : ascii) : bool:= 
   let n:= nat_of_ascii c in 
     andb (88 <=? n ) (n <=? 90).
 
-
+(*turns a string into a list of ascii characters*) 
 Fixpoint list_of_string (s : string) : list ascii :=
   match s with
   | EmptyString => []
   | String c s => c :: (list_of_string s)
   end.
 
+(*turns a list of ascii characters into a string *) 
 Definition string_of_list (xs : list ascii) : string :=
   fold_right String EmptyString xs.
 
+(* checks if a string is a valid site, need to change to accept any alpha numeric combination*) 
 Definition isSite (x : string) : bool :=
  if prefix "Q" x && forallb isDigit (list_of_string (substring 1 (length (list_of_string x)) x)) then  true
   else false.
 Compute index 0 "." "5.5". 
+
+(*modifying string index function*) 
 Fixpoint index (n : nat) (s1 s2 : string) {struct s2} : nat :=
   match s2, n with
   | EmptyString, 0 =>
@@ -76,15 +87,20 @@ Fixpoint index (n : nat) (s1 s2 : string) {struct s2} : nat :=
 Compute index 0 "." "5.5". 
 Compute negb (beq_nat 5 6). 
 
+(*checks if number passed in is one with a decimal*) 
 Definition isDecimalDigit (x: string) : bool := 
   if negb (beq_nat (index 0 "." x) 1000) && forallb isDigit (list_of_string (substring 0 (index 0 "." x) x)) && 
 forallb isDigit (list_of_string(substring ((index 0 "." x) + 1) (length (list_of_string x)) x) )then true 
 else false. 
 
 Compute isDecimalDigit "5". 
+
+(*checks if variable is a hamiltonian, similar to site, will change  to accept any alphanumeric combination*) 
 Definition isHam (x : string) : bool :=
  if prefix "H" x && forallb isDigit (list_of_string (substring 1 (length (list_of_string x)) x)) then  true
   else false.
+
+(*character types, classifying characters by character types*) 
 Inductive chartype := white | alpha | digit | period |  other.
 Definition classifyChar (c : ascii) : chartype :=
   if isWhite c then
@@ -97,13 +113,17 @@ Definition classifyChar (c : ascii) : chartype :=
     period
   else
     other.
+
+(*possible token keywords*) 
 Inductive token_keyword :=  empty | qubit_id | number| dot| left_brac | right_brac | colon | plus | subtr | divide | semicolon |kw_Site | kw_Ham| prog_id| kw_HPauli. 
 
+(*defining what  a token can contain*) 
 Inductive token_content := 
  | kw_tok (kw : token_keyword)
  | id_tok (id : string)
  | tok_num (num : option R)(raw_s : string).
 
+(*defining token*) 
 Record token:=
 make_token 
 {
@@ -121,6 +141,7 @@ Fixpoint eqb_string s1 s2 : bool :=
  end.
 Compute eqb_string "hello"%string "helpo"%string .
 
+(*determing what token keyword a given string is*) 
 Definition classifyKeyword (cs : string) : token_keyword :=
 match list_of_string cs with
 |[] => empty
@@ -158,14 +179,14 @@ match list_of_string cs with
 end.
 
 
-
+(*conversion from a string to a nat*) 
 Definition string_to_nat (s : string) : option nat :=
   match NilEmpty.uint_of_string s with
   | None => None
   | Some u => Some (Nat.of_uint u)
   end.
 
-
+(* turns a string to an option nat, modified from ImpParser*) 
 Definition parseNumber (xs : string)
                      : option nat :=
 match list_of_string xs with
@@ -191,7 +212,7 @@ Definition handle_token (tk : string) (n: nat) : token :=
   | _ => (make_token (kw_tok (classifyKeyword tk)) (id_tok tk%string) n)
   end. *)
 
-
+(*constructs list of substrings*) 
 Fixpoint tokenize_helper (cls : chartype) (acc xs : list ascii)
                        : list  (list ascii) := 
   let tk := match acc with [] => [] | _::_ => [rev acc] end in
@@ -225,7 +246,7 @@ Fixpoint tokenize_helper (cls : chartype) (acc xs : list ascii)
 Definition tokenize (s : string) : list string :=
   map string_of_list (tokenize_helper white [] (list_of_string s)).
 
-(*Deal with decimals later*) 
+(*Deal with decimals later, attempting to convert a string to a real number. Doesn't handle decimals, but could save index of decimal and compute real_number/10^{decimal_index}*) 
 Print NilEmpty.int_of_string.
 Definition string_to_R (s : string) : option R := 
  match NilEmpty.int_of_string s with 
@@ -233,14 +254,8 @@ Definition string_to_R (s : string) : option R :=
   | Some x => Some (IZR (Z.of_int x))
   end. 
 
-Definition get_R (s:string) : R := 
-  match string_to_R s with
-  | None => 0
-  | Some s => s
-  end. 
 
-Compute (get_R "63") + 10. 
-
+(*constructs a single token given classified token_content*) 
 Definition handle_token (tk:token_content) (n: nat) : token := 
   match tk with 
   | kw_tok kw =>  make_token (kw_tok kw) n
@@ -250,7 +265,7 @@ Definition handle_token (tk:token_content) (n: nat) : token :=
  
 
 Compute (handle_token (kw_tok kw_Ham) 0).
-
+(*makes list of tokens from list of substrings, takes same approach as ImpParser*) 
 Fixpoint make_token_list (kl: token_keyword) (tk: list string) (acc : list token) : (list token) := 
 let tkl := match acc with [] => [] | _ => rev acc end in 
  match tk with 
@@ -297,9 +312,11 @@ Compute (make_token_list (prog_id) (tokenize "HSF Site
 Hamiltonian
     [ H1 : 3.5 , Q1.X * Q2.Z + Q3.Y ]
     [ H2 : 4.245 , Q2.Y + Q1.Z ]
-    [ H3 : 4 , (2.4*3/5)Q1.X ]"%string) []).
+    [ H3 : 4 , (2.4*3/5)Q1.X ]"%string) []). (*correctly tokenizes a program*) 
 (**************************************************************** Parsing ****************************************************************)
 
+
+(*** This is an attempt to construct parser using same approach as ImpParser. We no longer believe this is the best way to go about this *) 
 Inductive optionE (X:Type) : Type :=
   | SomeE (x : X)
   | NoneE (s : string).
@@ -337,29 +354,16 @@ Fixpoint many_helper {T} (p : parser T) acc steps xs :=
 Definition many {T} (p : parser T) (steps : nat) : parser (list T) :=
   many_helper p [] steps.
 
-Compute nth 2  (make_token_list (prog_id) (tokenize "HSF Site
+Compute (make_token_list (prog_id) (tokenize "HSF Site
     qubit Q1
     qubit Q2
     qubit Q3 ;
 Hamiltonian
-    [ H1 : 3.5 , Q1.X * Q2.Z + Q3.Y ]
+    [ H1 : 3 , Q1.X * Q2.Z + Q3.Y ]
     [ H2 : 4.245 , Q2.Y + Q1.Z ]
-    [ H3 : 4 , (2.4*3/5)Q1.X ]"%string) []).
+    [ H3 : 4 + (2.4*3/5), Q1.X ]"%string) []).
 
 
-
-Definition getNthToken (tl: list token) (index : nat) (t : token) := 
- nth index tl t=t. 
-
-
-Compute getNthToken (make_token_list (prog_id) (tokenize "HSF Site
-    qubit Q1
-    qubit Q2
-    qubit Q3 ;
-Hamiltonian
-    [ H1 : 3.5 , Q1.X * Q2.Z + Q3.Y ]
-    [ H2 : 4.245 , Q2.Y + Q1.Z ]
-    [ H3 : 4 , (2.4*3/5)Q1.X ]"%string) []) 2.
 
 Definition printToken (t : token) : string := 
   match t.(token_val) with
@@ -368,6 +372,8 @@ Definition printToken (t : token) : string :=
   | tok_num x y => y 
   end.
 
+
+(*checks if two keywords are equivalent*) 
 Definition checkKeywordEquiv (x y : token_keyword) : bool :=
   match x, y with 
   | empty, empty  => true
@@ -399,7 +405,7 @@ Definition firstExpect {T} (t : token) (p : parser T)
                     | id_tok m => if string_dec z m then p xs' else NoneE ("expected '" ++ printToken t ++ "'.")
                     | _ =>  NoneE("expected '" ++ printToken t ++ " '.")
                     end
-                | kw_tok z => 
+                (*| kw_tok z => 
                     match t.(token_val) with 
                     | kw_tok m => if checkKeywordEquiv z m then p xs' else NoneE ("expected '" ++ printToken t ++ "'.")
                     | _ =>  NoneE("expected '" ++ printToken t ++ " '.")
@@ -408,22 +414,62 @@ Definition firstExpect {T} (t : token) (p : parser T)
                     match t.(token_val) with 
                     | tok_num n m => if string_dec y m then p xs' else NoneE ("expected '" ++ printToken t ++ "'.")
                     | _ =>  NoneE("expected '" ++ printToken t ++ " '.")
-                    end 
+                    end *)
+               | _ =>  NoneE ("expected '" ++ printToken t ++ "'.")
+
              end
             | [] =>
               NoneE ("expected '" ++ printToken t ++ "'.")
             end.
 
-Definition expect (t : token) : parser unit :=
+Definition expect1 (t : token) : parser unit :=
   firstExpect t (fun xs => SomeE (tt, xs)).
 
-Compute expect {| token_val := id_tok "Q3"; line_number := 0 |}. 
+Compute expect1 {| token_val := id_tok "Q3"; line_number := 0 |}. 
 
 Record HSF_program  := makeHSFProg {
-  prog_name: string;
-  prog_decls : list string;
+  prog_name: token_keyword;
+  prog_decls : list token_content;
   prog_insts : list string;
 }.
+
+(* change to allow any identifier, not just Q[num]* or H[num]* *)
+Definition parseIdentifier (xs : list token)
+                         : optionE (string * list token) :=
+match xs with
+| [] => NoneE "Expected identifier"
+| x::xs' =>
+  match x.(token_val) with 
+   | id_tok x' =>  if isSite x' || isHam x' then
+      SomeE ( (x'), xs')
+    else
+      NoneE ("Illegal identifier:'" ++ printToken x ++ "'")
+   | _ =>  NoneE ("Illegal identifier:'" ++ printToken x ++ "'")
+   end
+end.
+
+Compute parseIdentifier  [{| token_val := kw_tok prog_id; line_number := 0 |};{| token_val := id_tok "Q2"; line_number := 0 |}]. 
+
+Definition parseNum (xs : list token)
+                     : optionE (R * list token) :=
+match xs with
+| [] => NoneE "Expected number"
+| x::xs' =>
+    match x.(token_val) with 
+    | tok_num num str_num => 
+      match num with 
+      | None =>  NoneE "Expected number" 
+      | Some n =>  SomeE ( n, xs')
+      end 
+    | _ =>  NoneE "Expected number"
+    end 
+end.
+Compute string_to_R "5"%string. 
+
+Compute parseNum [{| token_val := tok_num (Some ((R1 + R1) * (R1 + R1))%R) "4"; line_number := 0 |};{| token_val := id_tok "Q2"; line_number := 0 |}].
+(***************************************** optionE version end ****************************************************) 
+
+
 
 Inductive compiler_error := makeCompilerError (msg: string). 
 
@@ -443,18 +489,18 @@ Definition  match_prog (tokens : list tokens) : HSF_prog :=
       := match_kw KW::ENDHSF tokens4 index4 status4.
 *)
 
-(*Returns next token in token list*) 
+(*checks if a token matches the given keyword. Can be thought of as a "next_token" function commonly used in a parser*) 
+Definition expect (tokens : list token) (kw : token_keyword) : option (list token) := 
+  match tokens with 
+  | head :: tail =>
+    match head.(token_val) with 
+    | kw_tok kw => Some tail (*match is successful, we can remove the head *) 
+    | _ => None (*not correct key word *) 
+    end
+  | _ => None (* no tokens to match*) 
+  end. 
 
-Compute firstExpect  {| token_val := kw_tok qubit_id; line_number := 0 |} parser. 
 
-Compute nth 2 (make_token_list (prog_id) (tokenize "HSF Site
-    qubit Q1
-    qubit Q2
-    qubit Q3 ;
-Hamiltonian
-    [ H1 : 3.5 , Q1.X * Q2.Z + Q3.Y ]
-    [ H2 : 4.245 , Q2.Y + Q1.Z ]
-    [ H3 : 4 , (2.4*3/5)Q1.X ]"%string) []).
 
 (* Prog := HSF id Sites Decls Hamiltonian Insts ENDHSF *) 
 
